a31b8a439d1f07124e58df79c8469c6d6fd958d3
Sidg612 <writetosid612@gmail.com>
2020-02-12 22:22:21 +0530

    Inital commit

---
+++ app/src/main/java/com/writetosid/suvidha/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.writetosid.suvidha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
