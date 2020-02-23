d91ff35ecb5ea680ac1fc0d80dd0ad7908cb5c16
Sidg612 <writetosid612@gmail.com>
2020-02-23 09:20:23 +0530

    Added some new layout

---
+++ app/src/main/java/com/writetosid/suvidha/HandlerAdapter.kt
@@ -0,0 +1,8 @@
+package com.writetosid.suvidha
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class HandlerAdapter(var handler:ArrayList<Handler>)
--- app/src/main/java/com/writetosid/suvidha/MainActivity.kt
+++ app/src/main/java/com/writetosid/suvidha/MainActivity.kt
@@ -3,7 +3,9 @@ package com.writetosid.suvidha
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.centre_panel.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ArrayAdapter.createFromResource(this,
+            R.array.Location,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerTV.adapter=adapter
+
+        }
+
+
+
+
+
     }
 }
+
+data class Handler(
+
+    var name:String,
+    var distnace: Int
+)
+
+
